d110153533f2a04a2af03277e0eeeaa07783aa7e
ivangrimaldi <ivan.grimaldi.to@gmail.com>
2014-11-20 11:19:22 +0100

    Added full CORS requests support

--- src/it/ismb/pert/dal/web/apis/rest/resources/BaseServerResource.java
+++ src/it/ismb/pert/dal/web/apis/rest/resources/BaseServerResource.java
@@ -1,11 +1,13 @@
 package it.ismb.pert.dal.web.apis.rest.resources;
 
 import org.restlet.engine.header.Header;
+import org.restlet.resource.Options;
 import org.restlet.resource.ServerResource;
 import org.restlet.util.Series;
 
 public class BaseServerResource extends ServerResource{
 	
+	//Adds custom header
 	protected void addCustomHeaders()
 	{
 		Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
@@ -13,7 +15,23 @@ public class BaseServerResource extends ServerResource{
 		    responseHeaders = new Series(Header.class);
 		    getResponse().getAttributes().put("org.restlet.http.headers", responseHeaders);
 		}
+		//allow request from other origins
 		responseHeaders.add(new Header("Access-Control-Allow-Origin", "*"));
 	}
 
+	//Method added for CORS requests support: pre-flight request use OPTION method
+	@Options
+	public void doOptions()
+	{
+		Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
+		if (responseHeaders == null) {
+		    responseHeaders = new Series(Header.class);
+		    getResponse().getAttributes().put("org.restlet.http.headers", responseHeaders);
+		}
+		responseHeaders.add("Access-Control-Allow-Origin", "*"); 
+	    responseHeaders.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    responseHeaders.add("Access-Control-Allow-Headers", "Content-Type"); 
+
+	    responseHeaders.add("Access-Control-Max-Age", "60"); 
+	}
 }
